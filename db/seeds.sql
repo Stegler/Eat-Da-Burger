d172a5d0271a24d97067e499c6d44adfcd0090d1
David Steger <d.steger@ymail.com>
2019-07-02 23:41:38 -0700

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Full Stack Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Above & Beyond Burger', false);
